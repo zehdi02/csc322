5c4b3c0f22f07df16d3e7c988f83eb01649e952f
Vbreezy173 <89701605+Vbreezy173@users.noreply.github.com>
2023-05-04 15:45:58 -0400

    Added connection to AWS db

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 import './SignUp.css'
 
 import MainLayout from '../layout/MainLayout'
 
 function SignUp() {
+
+  const [fnameReg, setfnameReg] = useState("");
+  const [lnameReg, setlnameReg] = useState("");
+  const [emailReg, setemailReg] = useState("");
+  const [passwordReg, setpasswordReg] = useState("");
+  const [phoneReg, setphoneReg] = useState("");
+
+  const regist = () => {
+    Axios.post("http://localhost:3001/sign-up", {
+      firstname: fnameReg,
+      lastname: lnameReg,
+      email: emailReg,
+      password: passwordReg,
+      phone: phoneReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+
   return (
     <MainLayout>
         <div class="signup-container">
                 <h1>Create an Account</h1>
                 <form class="signup-form" action="/signup" method="post">
-                    <input type="text" name="firstname" placeholder="First Name" required />
-                    <input type="text" name="lastname" placeholder="Last Name" required />
-                    <input type="email" name="email" placeholder="Email" required />
-                    <input type="password" name="password" placeholder="Password" required />
-                    <input type="password" name="confirm_password" placeholder="Confirm Password" required />
-                    <input type="tel" name="phone" placeholder="Mobile Phone Number" required />
-                    <button type="submit">Sign Up</button>
+                    <input type="text" name="firstname" placeholder="First Name" required onChange={(e) => {setfnameReg(e.target.value);}}/>
+                    <input type="text" name="lastname" placeholder="Last Name" required onChange={(e) => {setlnameReg(e.target.value);}}/>
+                    <input type="email" name="email" placeholder="Email" required onChange={(e) => {setemailReg(e.target.value);}}/>
+                    <input type="password" name="password" placeholder="Password" required onChange={(e) => {setpasswordReg(e.target.value);}}/>
+                    {/*<input type="password" name="confirm_password" placeholder="Confirm Password" required />*/}
+                    <input type="tel" name="phone" placeholder="Mobile Phone Number" required onChange={(e) => {setphoneReg(e.target.value);}}/>
+                    <button type="submit" onClick={regist} >Sign Up</button>
                 </form>
                 <p>Already have an account? <a href="/login">Sign in</a></p>
             </div>

a5814812d38e3823945cd0e149800eb342bcf2be
Rezwan1768 <96751801+Rezwan1768@users.noreply.github.com>
2023-05-05 17:34:30 -0400

    Added a Sample product detail page

---
+++ client/src/pages/Products/ProductDemo.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import cpu1 from '../../img/products_page/cpu1.png';
+import MainLayout from '../../layout/MainLayout'
+import './Product.css'
+function product() {
+    return (
+      
+        <MainLayout>
+            <div class="product-container">
+                <div class="image-container">
+                    <img src={cpu1} alt="Product 1" class="product-image"/>
+                </div>
+                <div class="product-info">
+                        <h1>ASUS - Vivobook 14" Laptop - Intel Core 11th Gen i3 with 8GB Memory - 128GB SSD - Transparent Silver</h1>
+                        
+                    <div class="product-description">
+                        <p> Include product description </p>
+                    </div>
+                    <div class="product-features">
+                        <p>Include product features </p>
+                    </div>
+                    <div className="name_buy">
+                        <p>$199.99</p>
+                        <a href="#" className="cta">
+                            <p>🛒 Add to Cart</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-reviews-container">
+                <div class="product-reviews">
+                    <h2>Product Reviews</h2>
+                    <p>Customer reviews and ratings will be displayed here.</p>
+                    <button>Write a Review</button>
+                </div>
+            </div>
+        </MainLayout>
+    )
+}
+export default product

0f0cb9df9cbfb699c3f0bb8c0d76ecf8ebee9171
Vbreezy173 <89701605+Vbreezy173@users.noreply.github.com>
2023-05-05 15:29:37 -0400

    connected signin page to db

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.post("/sign-in",(req,res)=>{
         console.log(err);
         res.send({err:err})
       }
-      if(result){
+      if(result.length >0){
         console.log(result);
         res.send(result);
       } else{

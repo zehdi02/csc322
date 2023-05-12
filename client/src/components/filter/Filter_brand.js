06c9ed9cb04914eefde746c164c6fd1deaccac0f
zehdi02 <de.melchizedek.castro@gmail.com>
2023-05-11 21:51:04 -0400

    Functioning Filter & Sort! Need to fix Rating filter.

--- client/src/components/filter/Filter_brand.js
+++ client/src/components/filter/Filter_brand.js
@@ -1,14 +1,30 @@
-import React, { createContext, useContext, useState } from 'react';
-import testData from '../browse/testData';
-import './Filter_cat.css'
+import React from 'react';
+import './Filter_cat.css';
 
-const Filter_brand = (props) => {
+const Filter_brand = ({ brand, selectedBrand, setSelectedBrand }) => {
+    const handleCategoryChange = (event) => {
+        console.log('handleCategoryChange called');
+        if (event.target.checked) {
+            setSelectedBrand([...selectedBrand, event.target.value]);
+        } else {
+            setSelectedBrand(
+            selectedBrand.filter((category) => category !== event.target.value)
+          );
+        }
+        console.log('selectedBrand:', selectedBrand);
+      };
     
     return (
         <div className="filter-brand">
             <label>
-                <input type="checkbox" name="cat" value="option1" />
-                {props.brand}
+                <input
+                type="checkbox"
+                name="brand"
+                value={brand}
+                checked={selectedBrand.includes(brand)}
+                onChange={handleCategoryChange}
+                />
+                {brand}
             </label>
         </div>
     )
